10c9a63adb15cbae19470daa91d66776bd6bec70
HenryZhuHR <HenryZhuHR@users.noreply.github.com>
2023-09-01 13:44:54 +0000

    deploy: HenryZhuHR/Homepage@f7ce692d20a08e44ac9bd589725969f33bdd7ec1

--- assets/2019_Shafahi_LS.html.ccf78b1c.js
+++
@@ -1,9 +0,0 @@
-import{o as e,c as n,F as i,a,d as r}from"./app.502a2083.js";import{_ as t}from"./plugin-vue_export-helper.21dcd24c.js";const o={},l=a("h2",{id:"label-smoothing-and-logit-squeezing-a-replacement-for-adversarial-training",tabindex:"-1"},[a("a",{class:"header-anchor",href:"#label-smoothing-and-logit-squeezing-a-replacement-for-adversarial-training","aria-hidden":"true"},"#"),r(" Label Smoothing and Logit Squeezing: {A} Replacement for Adversarial Training?")],-1),s=a("div",{class:"language-bibtex ext-bibtex"},[a("pre",{class:"language-bibtex"},[a("code",null,`@article{2019_Shafahi_LS,
-  author     = {Ali Shafahi and Amin Ghiasi and Furong Huang and Tom Goldstein},
-  title      = {Label Smoothing and Logit Squeezing: {A} Replacement for Adversarial Training?},
-  volume     = {abs/1910.11585},
-  year       = {2019},
-  url        = {http://arxiv.org/abs/1910.11585},
-  eprinttype = {arXiv}
-}
-`)])],-1);function d(c,g){return e(),n(i,null,[l,s],64)}var m=t(o,[["render",d]]);export{m as default};
